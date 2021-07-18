727695d13da1305892c8dbfed0c776f014dd9930
Amara <62611097+amaraahmedr3@users.noreply.github.com>
2021-07-18 07:55:12 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+function setup()
+{
+    //create a large square canvas
+    createCanvas(800, 800);
+}
+
+ 
+
+function draw()
+{
+
+ 
+
+    //Set the fill colour to white
+    fill(255,255,255);
+    
+    //set the fill colour to red
+    //fill(255, 0, 0);
+    
+        
+    
+    //set a thick stroke weight for the black lines
+    strokeWeight(15);
+
+ 
+
+    //draw the red rectangle 
+    rect(100, 0, 800, 600);
+    
+    //draw the white rectangle
+    rect (0,0,200,250);
+    
+    
+    //draw the white rectangle
+    rect (0,250,200,500);
+    
+    //draw the blue rectangle
+    rect(0,600,200,600);
+    
+    //draw the small blue rectangle
+    rect(600,600,700,700);
+    
+    //draw the small yellow rectangle
+    rect(600,700,700,750);
+    
+    
+   
+   
+
+}
