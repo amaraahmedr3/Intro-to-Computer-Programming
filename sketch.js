b076c2c998da58f697f0c7c23773996051740580
Amara <62611097+amaraahmedr3@users.noreply.github.com>
2021-07-18 08:33:55 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,60 +1,58 @@
 function setup()
 {
-    //create a large square canvas
-    createCanvas(800, 800);
+    //create a large square canvas
+    createCanvas(800, 800);
 }
- 
- 
- 
+
+ 
+
 function draw()
 {
- 
- 
- 
-    //Set the fill colour to white
-    fill(255,255,255);
-    
-    
-    //set a thick stroke weight for the black lines
-    strokeWeight(15);
- 
- 
-    //draw the large red rectangle 
-    rect(100, 0, 800, 600);
-    
-    
-    //fill red rectangle
-    //fill(255,255,255);
-    
-    //draw the white rectangle
-    rect (0,0,200,250);
-    
-    //fill the white rectangle
-    fill(255,0,0);
-    
-    
-    //draw the white rectangle
-    rect (0,250,200,500);
-    
-    //fill the white rectangle
-    fill(255,0,0);
-    
-    //draw the blue rectangle
-    rect(0,600,200,600);
-    
-    //fill the blue rectangle
-    fill(0,0,255);
-    
-    //draw the small blue rectangle
-    rect(600,600,700,700);
-    
-    //fill the blue rectangle
-    //fill(0,0,255);
-    
-    //draw the small yellow rectangle
-    rect(600,700,700,750);
-    
-    //fill the small yellow rectangle
-    //fill(255,255,0);
-    
+
+ 
+
+    //Set the fill colour to white
+    fill(255,255,255);
+    
+    
+    //set a thick stroke weight for the black lines
+    strokeWeight(15);
+
+ 
+    //draw the large red rectangle 
+    rect(100, 0, 800, 600);
+    
+    
+    //fill red rectangle
+    fill(255,255,255);
+    
+    //draw the white rectangle
+    rect (0,0,200,250);
+    
+    //fill the white rectangle
+    fill(255,250,250);
+    
+    
+    //draw the white rectangle
+    rect (0,250,200,500);
+    
+    //fill the white rectangle
+    fill(255,0,0);
+    
+    //draw the blue rectangle
+    rect(0,600,200,600);
+    
+    //fill the blue rectangle
+    fill(0,0,255);
+    
+    //draw the small white rectangle
+    rect(600,600,700,700);
+    
+    
+    //draw the small yellow rectangle
+    rect(600,700,700,750);
+    
+    //fill the small yellow rectangle
+    
+    
 }
