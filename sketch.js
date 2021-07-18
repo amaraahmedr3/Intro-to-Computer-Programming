69e68d1652c94c5ecb8ca8ccfb7db22760566696
Amara <62611097+amaraahmedr3@users.noreply.github.com>
2021-07-18 08:25:38 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,60 @@
 function setup()
 {
-    //create a large square canvas
-    createCanvas(800, 800);
+    //create a large square canvas
+    createCanvas(800, 800);
 }
-
- 
-
+ 
+ 
+ 
 function draw()
 {
-
- 
-
-    //Set the fill colour to white
-    fill(255,255,255);
-    
-    //set the fill colour to red
-    //fill(255, 0, 0);
-    
-        
-    
-    //set a thick stroke weight for the black lines
-    strokeWeight(15);
-
- 
-
-    //draw the red rectangle 
-    rect(100, 0, 800, 600);
-    
-    //draw the white rectangle
-    rect (0,0,200,250);
-    
-    
-    //draw the white rectangle
-    rect (0,250,200,500);
-    
-    //draw the blue rectangle
-    rect(0,600,200,600);
-    
-    //draw the small blue rectangle
-    rect(600,600,700,700);
-    
-    //draw the small yellow rectangle
-    rect(600,700,700,750);
-    
-    
-   
-   
-
+ 
+ 
+ 
+    //Set the fill colour to white
+    fill(255,255,255);
+    
+    
+    //set a thick stroke weight for the black lines
+    strokeWeight(15);
+ 
+ 
+    //draw the large red rectangle 
+    rect(100, 0, 800, 600);
+    
+    
+    //fill red rectangle
+    //fill(255,255,255);
+    
+    //draw the white rectangle
+    rect (0,0,200,250);
+    
+    //fill the white rectangle
+    fill(255,0,0);
+    
+    
+    //draw the white rectangle
+    rect (0,250,200,500);
+    
+    //fill the white rectangle
+    fill(255,0,0);
+    
+    //draw the blue rectangle
+    rect(0,600,200,600);
+    
+    //fill the blue rectangle
+    fill(0,0,255);
+    
+    //draw the small blue rectangle
+    rect(600,600,700,700);
+    
+    //fill the blue rectangle
+    //fill(0,0,255);
+    
+    //draw the small yellow rectangle
+    rect(600,700,700,750);
+    
+    //fill the small yellow rectangle
+    //fill(255,255,0);
+    
 }
